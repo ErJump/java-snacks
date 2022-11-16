3157ba844cafc731e0c82ec25ead9044cc5100e2
ErJump <fedelijunior@gmail.com>
2022-11-16 11:59:18 +0100

    feat: :bug: Fixes snack3

--- src/org/generation/italy/Snack3.java
+++ src/org/generation/italy/Snack3.java
@@ -2,12 +2,16 @@ package org.generation.italy;
 
 public class Snack3 {
 	public static void main(String[] args) {
+        
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
-        	if (numbers[i] % 2 != 0) {
-        		System.out.println(numbers[i]);
+        	if (i % 2 != 0) {
+        		sum += numbers[i];
         	}
         }
+        
+        System.out.println("sum: " + sum);
 	}
 }
