0673669f0f5c1bb8f5ffa7cdbc58d1bcff602e73
ErJump <fedelijunior@gmail.com>
2022-11-16 12:05:31 +0100

    feat: :sparkles: Snack4 completed
    Adds Snack4

---
+++ src/org/generation/italy/Snack4.java
@@ -0,0 +1,19 @@
+package org.generation.italy;
+
+import java.util.Random;
+
+public class Snack4 {
+	public static void main(String[] args) {
+		int[] arr1 = new int[4];
+		int[] arr2 = new int [10];
+		
+		Random rnd = new Random();
+		
+		int delta = arr2.length - arr1.length;
+		
+		for (int i = 0; i < delta; i++) {
+			System.out.println(rnd.nextInt());
+		}
+		
+	}
+}
