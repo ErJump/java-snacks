abc81c2facf2930dfdfc10284d184cfef1494ffe
ErJump <fedelijunior@gmail.com>
2022-11-16 09:36:33 +0100

    feat: :sparkles: Snack1 completed

---
+++ src/org/generation/italy/Snack1.java
@@ -0,0 +1,17 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class Snack1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Scegli un numero: ");
+		int userNumber = sc.nextInt();
+		
+		if (userNumber % 2 == 0) 
+			System.out.println(userNumber);
+		else
+			System.out.println(userNumber + 1);
+	}
+}
