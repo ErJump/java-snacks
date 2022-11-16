752e8738ad3385126423cc4cdb151fdfb9e4f7e6
ErJump <fedelijunior@gmail.com>
2022-11-16 17:30:37 +0100

    fix: :bug: Fixes min and max value in snack6

--- src/org/generation/italy/Snack6.java
+++ src/org/generation/italy/Snack6.java
@@ -15,28 +15,25 @@ public class Snack6 {
 		}
 		
 		//trovare il valore minimo e massimo all'interno di entrambi gli array
-		int min1 = arr1[0];
-		int max1 = arr1[0];
-		int min2 = arr2[0];
-		int max2 = arr2[0];
+		int min = arr1[0];
+		int max = arr1[0];
 
 		for (int i = 0; i < arr1.length; i++) {
-			if (arr1[i] < min1) {
-				min1 = arr1[i];
+			if (arr1[i] < min) {
+				min = arr1[i];
 			}
-			if (arr1[i] > max1) {
-				max1 = arr1[i];
+			if (arr1[i] > max) {
+				max = arr1[i];
 			}
-			if (arr2[i] < min2) {
-				min2 = arr2[i];
+			if (arr2[i] < min) {
+				min = arr2[i];
 			}
-			if (arr2[i] > max2) {
-				max2 = arr2[i];
+			if (arr2[i] > max) {
+				max = arr2[i];
 			}
 		}
 
-		System.out.println("min1 - max1: " + min1 + " - " + max1);
-		System.out.println("min2 - max2: " + min2 + " - " + max2);
+		System.out.println("min - max: " + min + " - " + max);
 
 		//calcolare la sommatoria e la media di entrambi gli array
 		int sum = 0; 
