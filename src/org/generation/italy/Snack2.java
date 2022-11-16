fa637f60dbeb08d18c5c730191ab274ef3f34a38
ErJump <fedelijunior@gmail.com>
2022-11-16 10:25:35 +0100

    refactor: :art: Adds list fullNames

--- src/org/generation/italy/Snack2.java
+++ src/org/generation/italy/Snack2.java
@@ -10,11 +10,18 @@ public class Snack2 {
         String[] surnames = {"Rossi", "Bianchi", "Verdi", "Paperoni", "Neri", "Fedeli", "Rizzo", "Monti", "Rosi", "Gialli"};
         
         Random rnd = new Random();
-
+        
+        String[] fullNames = new String[10];
+        
         for (int i = 0; i < 10; i++) {
         	int rndNameIndex = rnd.nextInt(names.length);
         	int rndSurnameIndex = rnd.nextInt(surnames.length);
-            System.out.println(names[rndNameIndex] + " " + surnames[rndSurnameIndex]);
+            String fullName = names[rndNameIndex] + " " + surnames[rndSurnameIndex];
+            fullNames[i] = fullName;
+        }
+        
+        for (int i = 0; i < fullNames.length; i++) {
+        	System.out.println(fullNames[i]);
         }
 	}
 }
